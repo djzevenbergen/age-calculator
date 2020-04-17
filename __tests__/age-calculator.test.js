1a97372ef82e4fd4f016c3506b9117b420e6ad7a
DJ Zevenbergen <djzevenbergen@gmail.com>
2020-04-17 09:40:57 -0700

    add other planet tests

--- __tests__/age-calculator.test.js
+++ __tests__/age-calculator.test.js
@@ -14,9 +14,34 @@ describe('ageCalc', function () {
 
   });
 
-  test('verifies that calculate mercYears returns the users in mercury years', function () {
 
+  // * Output: Mercury Age: 41.67
+  // Venus Age: 16.13
+  // Mars Age: 5.32
+  // Jupiter Age: .84
+
+
+  test('verifies that calculate mercYears returns the users age in mercury years', function () {
+    var mercuryYears = thisAge.mercYears(thisAge.age);
+    expect(mercuryYears).toEqual(41.67);
   });
+
+  test('verifies that venYears returns the users age in venus years', function () {
+    var venusYears = thisAge.venYears;
+    expect(venusYears).toEqual(16.13);
+  });
+
+  test('verifies that marYears returns the users age in Mars years', function () {
+    var marsYears = thisAge.marYears;
+    expect(marsYears).toEqual(5.32);
+  });
+
+  test('verifies that venYears returns the users age in venus years', function () {
+    var jupiterYears = thisAge.jupYears;
+    expect(jupiterYears).toEqual(.84);
+  });
+
+
 
 
 });
--- src/planets.js
+++ src/planets.js
@@ -3,4 +3,36 @@ export class Age {
     this.age = age;
   }
 
+  mercYears(years) {
+    const mercYears = .24;
+    let newYears = years / mercYears;
+    newYears = (Math.round(newYears * 100) / 100);
+    return newYears;
+  }
+
+  venYears(years) {
+    const venYears = .62;
+    let newYears = years / venYears;
+    newYears = (Math.round(newYears * 100) / 100);
+    return newYears;
+  }
+
+  marYears(years) {
+    const marYears = 1.88;
+    let newYears = years / marYears;
+    newYears = (Math.round(newYears * 100) / 100);
+    return newYears;
+
+  }
+
+  jupYears(years) {
+    const jupYears = 11.86;
+    let newYears = years / jupYears;
+    newYears = (Math.round(newYears * 100) / 100);
+    return newYears;
+
+  }
+
+
+  //( Math.floor(num * 100) / 100 )
 }
